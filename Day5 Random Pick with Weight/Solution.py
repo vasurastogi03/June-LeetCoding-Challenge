f39171469803cf0fae9ad60a70db124871216f9a
Vasu Rastogi <vasurastogi003@gmail.com>
2020-06-06 09:23:41 +0530

    Day 5

---
+++ Day5 Random Pick with Weight/Solution.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        """
+        :type w: List[int]
+        """
+        self.w = w
+        self.n = len(w)
+        for i in range(1, self.n):
+            w[i] += w[i-1]
+        self.s = self.w[-1]
+
+    def pickIndex(self) -> int:
+        """
+        :rtype: int
+        """
+        seed = random.randint(1,self.s)
+        l,r = 0, self.n-1
+        while l<r:
+            mid = (l+r)//2
+            if seed <= self.w[mid]:
+                r = mid
+            else:
+                l = mid+1
+        return l
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
