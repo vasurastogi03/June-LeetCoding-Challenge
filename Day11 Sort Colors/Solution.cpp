f3aced05ba1ca11de1b03346e5d231b0087ab109
Vasu Rastogi <vasurastogi003@gmail.com>
2020-06-12 08:54:31 +0530

    Day 11

---
+++ Day11 Sort Colors/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+        void sortColors(vector<int>& nums) {
+            int n = nums.size();
+            int second=n-1, zero=0;
+            for (int i=0; i<=second; i++) {
+                while (nums[i]==2 && i<second) swap(nums[i], nums[second--]);
+                while (nums[i]==0 && i>zero) swap(nums[i], nums[zero++]);
+            }
+        }
+};
